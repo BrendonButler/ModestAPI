826bba8c2ae6c5bb7c6549fa5ce9211d678d6a69
Brendon Butler <bbutler452@gmail.com>
2016-03-28 17:31:25 -0400

    tested titled logger

--- src/net/sparkzz/modest/net/sparkzz/test/Test.java
+++ src/net/sparkzz/modest/net/sparkzz/test/Test.java
@@ -1,6 +1,7 @@
 package net.sparkzz.modest.net.sparkzz.test;
 
 import net.sparkzz.modest.Modest;
+import net.sparkzz.modest.utils.Logger;
 
 /**
  * Created by Brendon Butler on 3/28/2016.
@@ -8,6 +9,8 @@ import net.sparkzz.modest.Modest;
 public class Test extends Modest {
 
 	public static void main(String[] args) {
+		log = new Logger("Test"); // creates a new logger with a title
+
 		log.print("test");
 	}
 }
